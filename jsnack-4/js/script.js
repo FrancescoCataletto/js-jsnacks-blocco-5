4e56e4b80da13a997ae494b3d42f1014e3341664
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-28 19:53:08 +0200

    function to search for the lightest bike

--- jsnack-4/js/script.js
+++ jsnack-4/js/script.js
@@ -21,11 +21,23 @@ const objArr = [
         peso: "6,8 Kg"
     },
     {
-        nome: "	Savadeck Phantom3.0",
+        nome: "Savadeck Phantom3.0",
         peso: "4,65 Kg"
     },
     {
-        nome: "	Savadeck Herd 6.0",
+        nome: "Savadeck Herd 6.0",
         peso: "5,9 Kg"
     }
 ]
+
+
+// funzione che pusha
+getWeight();
+function getWeight(){
+    let lightestBike;
+    for(let element of objArr){
+        element = objArr.reduce((min, obj) => (min.peso < obj.peso) ? min : obj);
+        lightestBike = element;
+    }
+    console.log(lightestBike);
+}
