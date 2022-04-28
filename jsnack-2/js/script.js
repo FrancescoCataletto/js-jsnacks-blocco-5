413aa1e20a9466bb4bd48398c311c8b7a235fd9d
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-28 15:50:39 +0200

    array of 10 objects

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -5,3 +5,57 @@ Crea 10 oggetti che rappresentano una zucchina.
 Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
  */
+// array degli oggetti
+
+const objArr = [
+    {
+        varietà: "Zucchino nero di Milano",
+        peso: "200 grammi",
+        lunghezza: "20cm"
+    },
+    {
+        varietà: "Zucchino romanesco",
+        peso: "300 grammi",
+        lunghezza: "23cm"
+    },
+    {
+        varietà: "Zucchino ortolano di Faenza",
+        peso: "250 grammi",
+        lunghezza: "14cm"
+    },
+    {
+        varietà: "Zucchina lunga fiorentina",
+        peso: "260 grammi",
+        lunghezza: "30cm"
+    },
+    {
+        varietà: "Zucchino siciliano",
+        peso: "100 grammi",
+        lunghezza: "15cm"
+    },
+    {
+        varietà: "Zucchina striata di Napoli",
+        peso: "270 grammi",
+        lunghezza: "17cm"
+    },
+    {
+        varietà: "Zucchina bianca triestina",
+        peso: "290 grammi",
+        lunghezza: "10cm"
+    },
+    {
+        varietà: "Zucchina rigata pugliese",
+        peso: "400 grammi",
+        lunghezza: "12cm"
+    },
+    {
+        varietà: "Zucchino tondo di Piacenza",
+        peso: "220 grammi",
+        lunghezza: "8cm"
+    },
+    {
+        varietà: "Zucchino tondo di Nizza",
+        peso: "170 grammi",
+        lunghezza: "9cm"
+    }
+]
