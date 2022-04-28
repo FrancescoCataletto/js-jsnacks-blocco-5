92c73359aef97d10d310698aee5dd84c348b098c
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-28 17:05:40 +0200

    array con bici da corsa

--- jsnack-4/js/script.js
+++ jsnack-4/js/script.js
@@ -5,3 +5,27 @@ Crea un array di oggetti, in cui ogni oggetto descrive una bici da corsa con le 
 Stampa a schermo la bici con peso minore utilizzando destructuring e template literal
 
  */
+
+// array delle bici
+const objArr = [
+    {
+        nome: "Pinarello Razha",
+        peso: "6,5 Kg"
+    },
+    {
+        nome: "Savadeck Warwind5.0",
+        peso: "8 Kg"
+    },
+    {
+        nome: "Savadeck Warwind3.0",
+        peso: "6,8 Kg"
+    },
+    {
+        nome: "	Savadeck Phantom3.0",
+        peso: "4,65 Kg"
+    },
+    {
+        nome: "	Savadeck Herd 6.0",
+        peso: "5,9 Kg"
+    }
+]
