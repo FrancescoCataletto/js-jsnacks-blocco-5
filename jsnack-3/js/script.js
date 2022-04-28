6ead389d2325b23eb1565d3f78126d1abfc731a5
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-28 16:53:26 +0200

    function to reverse a string

--- jsnack-3/js/script.js
+++ jsnack-3/js/script.js
@@ -5,3 +5,13 @@ Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (
 
 
  */
+reverseString("computer");
+
+
+function reverseString(param){
+    let reversedString = " ";
+    for(let i = param.length - 1; i >= 0; i--){
+        reversedString += param[i];
+    }
+    console.log(reversedString);
+}
